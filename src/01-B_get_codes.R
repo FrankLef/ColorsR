01a1f7dfcd4532713203b9b8af31f76c1aa49a83
FrankLef <flefebvre01@hotmail.com>
2022-07-10 08:29:07 -0400

    init

---
+++ src/01-B_get_codes.R
@@ -0,0 +1,10 @@
+# See the following resouces on the net to convert hex to rbg and
+# many others
+# https://www.rapidtables.com/convert/color/hex-to-rgb.html#:~:text=Hex%20to%20RGB%20conversion,get%20the%20green%20color%20level.
+# also, tis site is actually simpler and easier and it does i both ways
+# https://www.rgbtohex.net
+
+col_name <- "violet"
+col_hex <- gplots::col2hex(col_name)
+col_hex
+scales::show_col(col_hex)
